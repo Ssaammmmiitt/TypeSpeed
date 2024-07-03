e9652d105e071216fc315abf492fcd9a233c18f8
Sammit <poudyal.sammit@gmail.com>
2024-07-04 01:07:22 +0545

    feat: Update App component to display countdown timer and generated words
    The App component has been updated to include a countdown timer and a section for displaying generated words. This will enhance the user experience by providing a time limit for typing and showing the words to be typed.
    
    The countdown timer component has been added to display the remaining time in seconds. The generated words component has been created to render the words that the user needs to type.
    
    This commit also includes necessary project files and configurations.

--- typespeed/src/App.tsx
+++ typespeed/src/App.tsx
@@ -1,15 +1,30 @@
 import {faker} from "@faker-js/faker";
 
-const words = faker.random.words(10);
+const words = faker.random.words(40)
 console.log(words);
 
 
-
+const GeneratedWords=( {words} : {words: string } )=>{
+  return(
+    <div className="text-4xl text-center text-white italic">
+      {words}
+    </div>
+  )
+};
 
 function App() {
-  return <div className="text-4xl font-bold text-white text-center">
-    {words}
-  </div>;
+  return (
+    <>
+    <CountDownTimer timeLeft={60} />
+    <GeneratedWords words={words} />
+    </>
+  )
 }
+
+const CountDownTimer = ({ timeLeft }: { timeLeft: number }) => {
+   return (
+    <h2 className="text-primary-500 font-medium">Time: {timeLeft}</h2>
+   )
+};
 
 export default App;
---
+++ typespeed/src/components/RestartButton.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const RestartButton = ({
+  onRestart: handleRestart,
+  className = "",
+}: {
+  onRestart: () => void;
+  className?: string;
+}) => {
+  return <div>RestartButton</div>;
+};
+export default RestartButton;
